c9561ccbe818bc14415145b07b67bda0f964933b
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-11-04 12:45:17 +0100

    Dependencies installed + home page created

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React, { useState, useEffect, useRef, useContext } from "react";
+
+const Home = () => {
+  // COMPONENT VARIABLES
+  // CAMERA VARIABLES
+  // DARK MODE VARIABLES ?????
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
